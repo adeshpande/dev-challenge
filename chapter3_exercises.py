1e2a4a000dfdc83c0d72ee6e8a06e4564bf62bf4
Aniket Deshpande <adeshpande@ad01bos01.local>
2013-05-30 13:27:21 -0400

    10_day_challenge

--- chapter3_exercises.py
+++ chapter3_exercises.py
@@ -1,3 +1,30 @@
 # Exercises for chapter 3:
 
-# Name:
+# Name:Aniket Deshpande
+
+
+#3.1/3.2 -- Funtions are executed in order. By placing the after the call,  we cannot get the call. 
+
+#3.3
+
+def right_justify(string):
+    str_len = len(string)
+    spaces = 70 -  str_len
+    print " " * spaces + string
+
+right_justify('allen')
+
+
+line1 ="+----+----+"
+line2 ="|    |    |"
+
+print line1
+print line2
+print line2
+print line2
+print line1
+print line2
+print line2
+print line2
+print line1
+
